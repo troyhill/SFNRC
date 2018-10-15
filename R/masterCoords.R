da7b448d567be3ef8c293c8f92f8e91c4fb10e00
Troy Hill <hill.troy@gmail.com>
2018-10-15 10:32:38 -0400

    Create masterCoords.R

---
+++ R/masterCoords.R
@@ -0,0 +1,17 @@
+#' Data: Coordinates for water quality stations in DataForEver
+#'
+#'
+#'
+#' @format A dataframe with 3562 water quality stations:
+#' \describe{
+#' \item{stn}{station name}
+#' \item{long}{station longitude in decimal degrees}
+#' \item{lat}{station latitude in decimal degrees}
+#'}
+#' @docType data
+#' @keywords data, water quality
+#' @name masterCoords
+#' @usage masterCoords
+#' @examples ### export to .csv:
+#' write.csv(masterCoords, file = file.path(tempdir(), "masterCoords.csv"))
+NULL
