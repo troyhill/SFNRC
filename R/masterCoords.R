7880453157fbaf8f02d2c8288c9e1aa83b5ddecd
thill <thill@ibis.sfnrc.nps.gov>
2018-10-22 17:11:00 -0400

    masterCoords fixed

--- R/masterCoords.R
+++ R/masterCoords.R
@@ -1,8 +1,8 @@
-#' Data: Coordinates for water quality stations in DataForEver
+#' Data: Coordinates for water quality and hydrology stations in DataForEver
 #'
 #'
 #'
-#' @format A dataframe with 3562 water quality stations:
+#' @format A dataframe with 5109 monitoring stations. Coordinate reference system is "+init=epsg:3512".
 #' \describe{
 #' \item{stn}{station name}
 #' \item{long}{station longitude in decimal degrees}
@@ -14,4 +14,13 @@
 #' @usage masterCoords
 #' @examples ### export to .csv:
 #' write.csv(masterCoords, file = file.path(tempdir(), "masterCoords.csv"))
+#' 
+#' #### used to generate coordinate object
+#' \dontrun{
+#' library(dplyr)
+#' masterCoords <- read.csv(file = "/home/thill/RDATA/dataForEver/WQdata/station_coordinates/stnCoords_all.csv", stringsAsFactors = FALSE)
+#' masterCoords <- masterCoords %>% group_by(stn) %>% filter(duplicated(stn) | n()==1)
+#' coordinates(masterCoords) <- c("long", "lat")
+#' proj4string(masterCoords) <- CRS("+init=epsg:3512")
+#' }
 NULL
