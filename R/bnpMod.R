9051c511bd9af061ddea2b5d543deb8ebf7362df
thill <thill@ibis.sfnrc.nps.gov>
2018-10-23 12:40:03 -0400

    ver 0.0.4

---
+++ R/bnpMod.R
@@ -0,0 +1,18 @@
+#' Data: Biscayne Bay model boundaries
+#'
+#'
+#'
+#' @format A shapefile of model boundaries in Biscayne Bay, Florida. Coordinate reference system is "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"
+#' \describe{
+#' \item{HECTARES}{area of basin in hectares}
+#' \item{BOX_CODE}{basin code}
+#' \item{Box_Basin}{name of basin}
+#'}
+#' @docType data
+#' @keywords biscayne bay, map
+#' @name bnpMod
+#' @usage bnpMod
+#' @examples 
+#' plot(bnpMod)
+#' 
+NULL
