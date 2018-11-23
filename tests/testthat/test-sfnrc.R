c7ac78437607fe0706bcbd8881ac732708a5605c
thill <thill@ibis.sfnrc.nps.gov>
2018-11-23 14:42:56 -0500

    tests

--- tests/testthat/test-sfnrc.R
+++ tests/testthat/test-sfnrc.R
@@ -3,6 +3,19 @@ library(SFNRC)
 
 
 
+test_that("se gives correct output", {
+  expect_equal(se(10), NA)
+  expect_equal(se(c(5, 5, 5, NA)), 0)
+})
+
+test_that("geoMean gives correct output", {
+  expect_equal(geoMean(10), 10)
+  expect_equal(geoMean(c(15, 15, 15, NA)), 15)
+  expect_error(geoMean(c(15, 15, 15, NA), nas = FALSE)) 
+  expect_equal(geoMean(c(15, 15, 15, NA), nas = TRUE, zero.propagate = TRUE), 15)
+})
+
+
 # test_that("fld.frq gives correct output", {
 #   expect_equal(fld.frq(2, 1:10, units = "percent"), 0.8)
 #   expect_equal(fld.frq(2, 1:10, units = "tides"), 9)
