fc5dcfeab4bc05c240d6ab755ee8e8c86e2b083d
Troy Hill <Hill.Troy@gmail.com>
2018-11-24 13:20:13 -0500

    getWQ test added

--- tests/testthat/test-sfnrc.R
+++ tests/testthat/test-sfnrc.R
@@ -18,10 +18,9 @@ test_that("interp runs", {
 })
 
 
-# test_that("getStn breaks", { 
-#   a <- getStn()
-#   expect_error()
-# })
+test_that("getWQ breaks", {
+  expect_error(getWQ())
+})
 
 
 test_that("se gives correct output", { 
