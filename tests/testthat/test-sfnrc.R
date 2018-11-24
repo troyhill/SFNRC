84816104c08a4cc265f259637a66d41d6c6077da
Troy Hill <Hill.Troy@gmail.com>
2018-11-24 12:19:47 -0500

    checking if codecov syncs with CI

--- tests/testthat/test-sfnrc.R
+++ tests/testthat/test-sfnrc.R
@@ -1,12 +1,21 @@
 context("test-sfnrc")
 library(SFNRC)
 
-# test_that("interp runs", {
-#   expect_that(a <- plyr::join_all(list(finDat, as.data.frame(masterCoords)));
-#               coordinates(a) <- c("long", "lat");
-#     interp(inputData = a, by = "stn", 
-#               paramCol = "param", year = "2016") )
-# }) 
+
+test_that("interp runs", {
+  a <- plyr::join_all(list(finDat, as.data.frame(masterCoords)));
+  a <- a[!is.na(a$long), ]
+  coordinates(a) <- c("long", "lat")
+  proj4string(a) <- CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")
+  
+  a.interp <- interp(inputData = a, by = "stn", returnRas = TRUE,
+                     paramCol = "value", year = "2016")
+  
+  expect_true(typeof(a.interp) == "S4")
+  expect_true(typeof(interp(inputData = a, by = "stn", returnRas = TRUE,
+                            interpMethod = "nearest neighbor",
+                            paramCol = "value", year = "2016")) == "S4")
+})
 
 test_that("se gives correct output", { 
   expect_equal(se(c(5, 5, 5, NA)), 0)
