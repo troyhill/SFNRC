8aceeb904138c355e20962c70e00fcc85031e736
troyhill <hill.troy@gmail.com>
2024-08-25 10:27:11 -0400

    Added downloadNWS and summarizeNWS

---
+++ R/downloadNWS.R
@@ -0,0 +1,120 @@
+#' @title Function bulk downloads National Weather Service precipitation geotiff files
+#'
+#' @description Useful for automating download of large numbers of NWS spatial precipitation layers, and easily download new data. Stage 3 and Stage 4 data can be found at https://water.noaa.gov/resources/downloads/precip/
+#'
+#' @param data_folder parent directory for data to be deposited. If this does not exist, it will be created.
+#' @param overwrite_data TRUE/FALSE value indicating whether existing files should be overwritten. If TRUE, all data are downloaded fresh.
+#' @param stage type of data to download. Acceptable values include `stage3` and/or `stage4`.
+#' @param stage3_filename example of a stage 3 filename. YYYYMMDD is a token that will be replaced with actual dates. Adjusting this can accommodate odd filenames (e.g., 2017)
+#' @param stage4_filename example of a stage 4 filename. YYYYMMDD is a token that will be replaced with actual dates.
+#' @param day_range numeric range of days to download from each month. Default only downloads data from the first of the month.
+#'
+#' @return NULL \code{downloadNWS} downloads files to the provided `data_folder` directory. 
+#' 
+#' @importFrom terra rast
+#' @importFrom terra writeRaster
+#' 
+#' @export
+#'
+#' @examples
+#' 2+2
+downloadNWS <- function(data_folder,
+                         overwrite_data = FALSE, # if TRUE, all data is downloaded fresh
+                         stage = c('stage3', 'stage4'), # type of data to download
+                         stage3_filename = 'nws_precip_mtd_YYYYMMDD.tif', # example of a stage 3 filename. YYYYMMDD is a token that will be replaced with actual dates.
+                         stage4_filename = 'nws_precip_mtd_YYYYMMDD_conus.tif',
+                         day_range       = 1 # range of days to download from each month. Default only downloads data from the first of the month
+) {
+  filename <- c(stage3_filename, # stage 3 filename pattern
+                stage4_filename) # stage 4 filename pattern
+  
+  if (any(grepl(x = stage, pattern = '3'))) {
+    if (!dir.exists(file.path(data_folder, 'stage3'))) {
+      dir.create(file.path(data_folder, 'stage3'), recursive = TRUE)
+    }
+    ### create list of dates, then urls
+    years <- as.character(2005:2017)
+    months<- c('01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12')
+    days  <- as.character(day_range)
+    days[nchar(days) < 2] <- paste0('0', days[nchar(days) < 2])
+    targetDates <- expand.grid(years, months, days)
+    targetDates$ymd  <- paste0(targetDates$Var1, targetDates$Var2, targetDates$Var3)
+    targetDates      <- targetDates[!grepl(x = targetDates$ymd, pattern = '20050101'), ]
+    targetDates$ymd2 <- paste(targetDates$Var1, targetDates$Var2, targetDates$Var3, sep = '/')
+    targetDates$url  <- paste0('https://water.noaa.gov/resources/downloads/precip/stageIII/', targetDates$ymd2, '/', mapply(gsub, filename[1], pattern = 'YYYYMMDD', replacement = targetDates$ymd, USE.NAMES = FALSE))
+    targetDates$basename <- basename(targetDates$url)
+    targetDates$download_location <- file.path(data_folder, 'stage3', targetDates$basename)
+    existing_files <- list.files(file.path(data_folder, 'stage3'), full.names = FALSE)
+    targetDates$exists_in_directory <- ifelse(targetDates$basename %in% existing_files, 1, 0)
+    
+    if (overwrite_data == FALSE) {
+      targetDates <- targetDates[targetDates$exists_in_directory == 0, ] # remove existing data from list of impending downloads
+    }
+    
+    ### now download and save
+    for (i in 1:nrow(targetDates)) {
+      message('Downloading Stage 3 data for ', targetDates$ymd2[i])
+      tryCatch(
+        tmp <- suppressWarnings(terra::rast(targetDates$url[i])), 
+        error = function(cond) {
+          message(paste("URL does not seem to exist:", targetDates$url[i]))
+        }
+      )
+      if (exists('tmp')) {
+        terra::writeRaster(x = tmp, filename = targetDates$download_location[i], overwrite = TRUE)
+        rm(tmp)
+      }
+    }
+    message('Stage 3 data download complete!')
+    # tst <- terra::rast(targetDates$download_location[i])  
+  }
+  if (any(grepl(x = stage, pattern = '4'))) {
+    if (!dir.exists(file.path(data_folder, 'stage4'))) {
+      dir.create(file.path(data_folder, 'stage4'), recursive = TRUE)
+    }
+    ### create list of dates, then urls. July 2016 is the first month that follows this pattern. Jan-Jun 2016 can be downloaded manually.
+    years <- as.character(2016:as.numeric(format(Sys.Date(), format = '%Y')))
+    months<- c('01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12')
+    days  <- as.character(day_range)
+    days[nchar(days) < 2] <- paste0('0', days[nchar(days) < 2])
+    targetDates <- expand.grid(years, months, days)
+    targetDates$ymd  <- paste0(targetDates$Var1, targetDates$Var2, targetDates$Var3)
+    ### remove first part of 2016
+    targetDates      <- targetDates[!(targetDates$ymd %in% paste0('20160',1:6, '01')), ]
+    ### remove dates in current year up to two weeks ago
+    current_mo       <- format(Sys.Date() - 14, format = '%m')
+    available_months <- 1:as.numeric(current_mo)
+    months_to_remove <- as.character(c(1:12)[!(c(1:12) %in% available_months)])
+    months_to_remove[nchar(months_to_remove) == 1] <- paste0('0', months_to_remove[nchar(months_to_remove) == 1])
+    dates_to_remove  <- paste0(format(Sys.Date(), format = '%Y'), months_to_remove, '01')
+    targetDates      <- targetDates[!(targetDates$ymd %in% dates_to_remove), ]
+    
+    targetDates$ymd2 <- paste(targetDates$Var1, targetDates$Var2, targetDates$Var3, sep = '/')
+    targetDates$url  <- paste0('https://water.noaa.gov/resources/downloads/precip/stageIV/', targetDates$ymd2, "/", mapply(gsub, filename[2], pattern = 'YYYYMMDD', replacement = targetDates$ymd, USE.NAMES = FALSE))
+    targetDates$basename <- basename(targetDates$url)
+    targetDates$download_location <- file.path(data_folder, 'stage4', targetDates$basename)
+    existing_files   <- list.files(file.path(data_folder, 'stage4'), full.names = FALSE)
+    targetDates$exists_in_directory <- ifelse(targetDates$basename %in% existing_files, 1, 0)
+    
+    if (overwrite_data == FALSE) {
+      targetDates <- targetDates[targetDates$exists_in_directory == 0, ] # remove existing data from list of impending downloads
+    }
+    
+    ### now download and save
+    for (i in 1:nrow(targetDates)) {
+      message('Downloading Stage 4 data for ', targetDates$ymd2[i])
+      tryCatch(
+        tmp <- suppressWarnings(terra::rast(targetDates$url[i])), 
+        error = function(cond) {
+          message(paste("URL does not seem to exist:", targetDates$url[i]))
+        }
+      )
+      if (exists('tmp')) {
+        terra::writeRaster(x = tmp, filename = targetDates$download_location[i], overwrite = TRUE)
+        rm(tmp)
+      }
+    }
+    message('Stage 4 data download complete!')
+  }
+  invisible()
+}
---
+++ R/summarizeNWS.R
@@ -0,0 +1,104 @@
+#' @title Function summarizes National Weather Service precipitation geotiff files by polygon
+#'
+#' @description Summarize spatial data using a polygon layer to return raw values for each date and aggregate measures by month.
+#'
+#' @param tif_addresses character vector with full file names for tiff files to be summarized (including entire directory and file extension). Can be created with `list.files('C:/directory', full.names = TRUE)`.
+#' @param input_polygon Spatvector with polygons used to summarize precipitation data. Load with `terra::vect`. Example polygon data: https://catalog.data.gov/dataset/tiger-line-shapefile-2019-state-georgia-current-county-subdivision-state-based
+#' @param polygon_names character. Name of column in `input_polygon` containing feature names/IDs. If left as `NULL`, features are numbered by their row in the polygon object.
+#' @param create_plots logical. Optional visualization produced for each layer. Setting this to `TRUE` will dramatically slow down the run time.
+#' @param func function. Used to summarize the monthly data from each polygon.
+#' @param date_correction numeric. Optional correction for data date. If no correction is desired, set to 0.
+#'
+#' @return list \code{summarizeNWS} containing (1) `summary_table`, a summary dataframe showing the result of `func` applied to monthly values for Jan-Dec. More complex operations can be performed on the raw values, and (2) `all_data`, a dataframe containing the raw values from each polygon and each layer.
+#' 
+#' @importFrom terra rast
+#' @importFrom terra extract
+#' @importFrom terra plot
+#' @importFrom terra project
+#' @importFrom terra crs
+#' @importFrom terra crop
+#' @importFrom terra extract
+#' @importFrom terra values
+#' @importFrom stats aggregate
+#' @importFrom stats reshape
+#' 
+#' @export
+#'
+#' @examples
+#' 2+2
+summarizeNWS <- function(tif_addresses,
+                         input_polygon, # shapefile, e.g., 
+                         polygon_names = NULL, # optional; name of the column in the polygon object with feature IDs/names
+                         create_plots = FALSE,
+                         func = median, # value to report in the final summary table (max, min, mean, etc.)
+                         date_correction = -1 # optional correction for data date. 
+) {
+  
+  if (!is.null(polygon_names)) {
+    ### confirm polygon names are present
+    if (!polygon_names %in% names(input_polygon)) {
+      polygon_names <- NULL
+    }
+  }
+  if (is.null(polygon_names)) {
+    polygon_names <- 1:nrow(input_polygon)
+  }
+  
+  for (i in 1:length(tifs)) {
+    tif_address <- tifs[i]
+    ### get date
+    split_file_name <- strsplit(x = gsub(x = basename(tif_address), pattern = 'conus|pr|ak|\\.tif', replacement = ''), split = '_')[[1]]
+    date_value      <- split_file_name[length(split_file_name)]
+    
+    ### get data
+    dat1 <- terra::rast(tif_address)
+    input_polygon <- terra::project(input_polygon, terra::crs(dat1, proj = TRUE))
+    
+    dat1 <- dat1[[grep(x = names(dat1), pattern = '_1|Observed')]]
+    dat1 <- terra::crop(dat1, input_polygon, mask = TRUE)
+    
+    if(create_plots) {
+      terra::plot(dat1, main = paste0('Pixel-level rainfall:\n', as.character(as.Date(date_value, format = '%Y%m%d') - 1)))
+      terra::plot(input_polygon, add = TRUE)
+    }
+    
+    ### get pixel values for each county
+    extracted.int <- sapply(dat1, function(x) {
+      x.df <- data.frame(t(terra::extract(x, y = input_polygon, 
+                                          fun = mean, na.rm = TRUE)))
+      x.df <- x.df[!grepl(x = row.names(x.df), pattern = "ID"), 
+      ]
+    })
+    extracted.int           <- data.frame(extracted.int)
+    names(extracted.int)[1] <- paste0('precip', # split_file_name[length(split_file_name) - 2],
+                                      '_', split_file_name[length(split_file_name) - 1])
+    extracted.int$monthYear <- as.Date(date_value, format = '%Y%m%d') + date_correction # using -1 is useful for full-month rainfall; changes date to last day of prior month. Assuming that e.g., June 1st reflects May rainfall totals
+    extracted.int$month     <- format(extracted.int$monthYear, '%b')
+    extracted.int$name      <- terra::values(input_polygon[, polygon_names])[,1]
+    
+    if(create_plots) {
+      input_polygon$mtd <- extracted.int[,1]
+      terra::plot(input_polygon, 'mtd', main = paste0('Polygon-level rainfall:\n', as.character(as.Date(date_value, format = '%Y%m%d') - 1)))
+    }
+    if (i == 1) {
+      finDat <- extracted.int
+    } else {
+      finDat <- rbind(finDat, extracted.int)
+    }
+  }
+  
+  ### now create a table
+  tbl1 <- stats::aggregate(unlist(get(names(finDat)[1])) ~ month + name, data = finDat, 
+                           FUN = function(x) func(x, na.rm = TRUE))
+  names(tbl1)[3] <- names(finDat)[1]
+  tbl2 <- stats::reshape(tbl1, idvar = "name", timevar = "month", direction = "wide")
+  names(tbl2)[2:ncol(tbl2)] <- substr(names(tbl2)[2:ncol(tbl2)], nchar(names(tbl2)[2:ncol(tbl2)])-2, nchar(names(tbl2)[2:ncol(tbl2)]))
+  tbl2 <- tbl2[, c('name', month.abb[month.abb %in% names(tbl2)[2:ncol(tbl2)]])]
+  head(tbl2)
+  row.names(tbl2) <- 1:nrow(tbl2)
+  
+  ### return the table
+  return(list(summary_table = tbl2,  # monthly means
+              all_data      = finDat # raw data, long format
+  ))
+}
